886bb4fc1085edbbc7dcf93b966fe738438c2f00
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2015-02-18 22:41:57 +0000

    Some more tidy up on the map service.

--- app/mapService.js
+++ app/mapService.js
@@ -12,7 +12,11 @@ angular
   ]);
 
 function service($compile){
-  var ms = {},
+  // check openlayers is available on service instantiation
+  // this can be handled with Require later on
+  if (!ol) return {};
+
+  var map = {}, //convenience reference
     defaults = {
       zoom: 15,
       startLocation: [0,40],
@@ -23,14 +27,92 @@ function service($compile){
     },
     zIndex = 9999, 
     popup, 
-    selectedFeature;
+    selectedFeature,
+    myZoomToExtentControl;
   
-  ms.map = {};
+  // public API
+  var ms = {
+    map: map, // ol.Map
+    init: init,
+    getFeatures: getFeatures,
+    selectFeature: selectFeature,
+    hideFeatures: hideFeatures
+  };
   
-  //check openlayers is available on service instantiation
-  if (!ol) return ms;
+  return ms;
 
-  ms.hideFeatures = function(features, search){
+  function getFeatures() {
+    var f = []; 
+    map  //ol.Map
+      .getLayers()  //ol.Collection
+      .getArray()[1]  //ol.layer.Vector
+        .getSource()  //ol.source.KML
+          .getFeatures()  //ol.Feature
+          .forEach(function(olFeature, i) {
+            var feature = {id: olFeature.getId()};
+            f.push(mapFeatureProperties(feature, olFeature));
+          });
+    return f;
+  }
+  
+  function selectFeature(name, pan){
+    var feature;
+    if (!name) return;
+    var target = $("#map path[feature='" + escape(name) + "']")[0];
+    
+    //search for feature
+    map  //ol.Map
+    .getLayers()  //ol.Collection
+    .getArray()[1]  //ol.layer.Vector
+      .getSource()  //ol.source.KML
+        .getFeatures()
+        .forEach(function(item, i) {
+          var f = item.get('name');
+          if (name==f)
+            feature = item;
+        });
+    selectedFeature = feature;
+    
+    if (feature) {
+      $("#map path").each(function(index, item){
+        item.setAttribute("class", "icon");
+      });
+      target.setAttribute("class", "icon selected");
+      
+      //put on top
+      $(target.parentNode.parentNode)
+        .parent().parent()
+        .css('z-index', ++zIndex);
+    }
+
+    //display feature details and pan
+    if (pan && feature) {
+      onFeatureSelected(feature);
+      panToFeature(feature, map.getView().getZoom());
+    
+      var element = angular.element('#popup');
+      $(element).popover('destroy');
+      //show popup for feature or hide any previous one
+      if (feature) {
+        setTimeout(function(){
+          var coord = feature.getGeometry().getCoordinates();
+          var title = feature.get('name');
+          popup.setPosition(coord);
+          $(element).popover({
+            'title': title, 
+            'placement': 'top',
+            'animation': false,
+            'html': true
+            //'content': feature.get('description')
+          });
+          $(element).popover('show'); 
+        }, 1000);
+      }
+    }
+    return feature;
+  }
+
+  function hideFeatures(features, search){
     //hide any popups
     var element = angular.element('#popup');
     $(element).popover('destroy');
@@ -53,64 +135,9 @@ function service($compile){
 	  });
   }
   
-  ms.selectFeature = function(name, pan){
-    var feature;
-    if (!name) return;
-    var target = $("#map path[feature='" + escape(name) + "']")[0];
-    
-    //search for feature
-    ms.map	//ol.Map
-    .getLayers()	//ol.Collection
-    .getArray()[1]	//ol.layer.Vector
-    	.getSource()	//ol.source.KML
-  		  .getFeatures()
-  		  .forEach(function(item, i) {
-  		    var f = item.get('name');
-  		    if (name==f)
-  		      feature = item;
-  	  });
-  	  selectedFeature = feature;
-  	  
-  	  if (feature) {
-  	    $("#map path").each(function(index, item){
-  	      item.setAttribute("class", "icon");
-  	    });
-  	    target.setAttribute("class", "icon selected");
-  	    
-  	    //put on top
-        $(target.parentNode.parentNode)
-          .parent().parent()
-          .css('z-index', ++zIndex);
-  	  }
 
-  	  //display feature details and pan
-  	  if (pan && feature) {
-        ms.onFeatureSelected(feature);
-			  ms.panToFeature(feature, ms.map.getView().getZoom());
-			
-  	    var element = angular.element('#popup');
-    	  $(element).popover('destroy');
-  			//show popup for feature or hide any previous one
-  			if (feature) {
-  				setTimeout(function(){
-  					var coord = feature.getGeometry().getCoordinates();
-            var title = feature.get('name');
-    				popup.setPosition(coord);
-    				$(element).popover({
-    				  'title': title, 
-    				  'placement': 'top',
-              'animation': false,
-              'html': true
-    				  //'content': feature.get('description')
-    				});
-    				$(element).popover('show');	
-  				}, 1000);
-  			}
-			}
-			return feature;
-  }
   
-  ms.mapFeatureProperties = function(feature, olFeature) {
+  function mapFeatureProperties(feature, olFeature) {
     if (!olFeature) return feature;
     if (!feature) feature = {};
     defaults.featurePropertiesMap.forEach(function(key){
@@ -119,26 +146,14 @@ function service($compile){
     return feature;
   }
   
-  ms.onFeatureSelected = function(olFeature) {
+  function onFeatureSelected(olFeature) {
     if (!olFeature) return;
-    var feature = ms.mapFeatureProperties({}, olFeature);
+    var feature = mapFeatureProperties({}, olFeature);
     if(defaults.onFeatureSelected)
       defaults.onFeatureSelected(feature);
   }
   
-  ms.getFeatures = function () {
-    var f = []; 
-    ms.map	//ol.Map
-  	  .getLayers()	//ol.Collection
-  	  .getArray()[1]	//ol.layer.Vector
-  	  	.getSource()	//ol.source.KML
-  			  .getFeatures()  //ol.Feature
-  			  .forEach(function(olFeature, i) {
-  			    var feature = {id: olFeature.getId()};
-  			    f.push(ms.mapFeatureProperties(feature, olFeature));
-  			  });
-    return f;
-  }
+
   
   // Creates an overlays in the given coordinates
   function createSVGOverlay(position, feature) {
@@ -171,12 +186,12 @@ function service($compile){
       });
   }
   
-  ms.renderSVGFeatures = function(){
+  function renderSVGFeatures(){
     if (defaults.extractStylesKml) return;
     
     //wait till directive renders svg element
     setTimeout(function() {
-      ms.map	//ol.Map
+      map	//ol.Map
     	  .getLayers()	//ol.Collection
     	  .getArray()[1]	//ol.layer.Vector
     	  	.getSource()	//ol.source.KML
@@ -186,13 +201,13 @@ function service($compile){
     			    if (!hidden) {
                 var coordinates = item.getGeometry().getCoordinates();
                 var overlay = createSVGOverlay(coordinates, item);
-                ms.map.addOverlay(overlay);
+                map.addOverlay(overlay);
     			    }
             });      
     }, 0);
   }
   
-  ms.popupSetup = function() {
+  function popupSetup() {
     var element = angular.element('#popup');
     
     // Add popup showing the position the user clicked
@@ -201,7 +216,7 @@ function service($compile){
       stopEvent: true,
 			offset: defaults.popupOffset
     });
-    ms.map.addOverlay(popup);
+    map.addOverlay(popup);
     
     var displayPopup = function(evt){
       var element = popup.getElement();
@@ -222,10 +237,10 @@ function service($compile){
     }
     
 		// display popup on click
-		ms.map.on('click', function(evt) {
+		map.on('click', function(evt) {
 		  if (defaults.extractStylesKml) {
 		    // Regular rendered feature find on click coordinates
-  			var feature = ms.map.forEachFeatureAtPixel(evt.pixel,
+  			var feature = map.forEachFeatureAtPixel(evt.pixel,
   				function(feature, layer) {
   					return feature;
   			});
@@ -234,13 +249,13 @@ function service($compile){
   		  if (!feature && evt.originalEvent.target && evt.originalEvent.target.nodeName == "path") {
           var target = evt.originalEvent.target;
           var featureId = unescape(target.getAttribute('feature'));
-          feature = ms.selectFeature(featureId, false);
+          feature = selectFeature(featureId, false);
   		  };
 		  };
 		  
 		  //trigger onFeatureSelected event
 		  selectedFeature = feature;
-		  ms.onFeatureSelected(feature);
+		  onFeatureSelected(feature);
 			
 			$(element).popover('destroy');
 			//show popup for feature or hide any previous one
@@ -262,39 +277,41 @@ function service($compile){
 			}
 			
 			if (feature) {
-			  ms.panToFeature(feature, ms.map.getView().getZoom());
+			  panToFeature(feature, map.getView().getZoom());
 			} 
 		});
 		
   }
   
-  ms.panToFeature = function(feature, zoom) {
+  function panToFeature(feature, zoom) {
 		var lonLat = feature.getGeometry().getCoordinates()
 		
-		var olPixel = ms.map.getPixelFromCoordinate(lonLat);
+		var olPixel = map.getPixelFromCoordinate(lonLat);
 		olPixel[1] -= 40;
-		lonLat = ms.map.getCoordinateFromPixel(olPixel);
+		lonLat = map.getCoordinateFromPixel(olPixel);
 		
-		if (ms.map.getView().getZoom() < zoom) 
-			ms.map.getView().setZoom(zoom);
+		if (map.getView().getZoom() < zoom) 
+			map.getView().setZoom(zoom);
 		
 		var animation = ol.animation.pan({
 		  duration: 1000,
 			easing: eval(ol.easing.inAndOut),
-			source: ms.map.getView().getCenter()
+			source: map.getView().getCenter()
 		});
 			
 		// Add animation to the render pipeline
-		ms.map.beforeRender(animation);
+		map.beforeRender(animation);
 		// Change center location
-		ms.map.getView().setCenter(lonLat);
+		map.getView().setCenter(lonLat);
 	};
   
-  ms.init = function(c){
-    var config = angular.extend(defaults, c);
+  function init(config){
+    var config = angular.extend(defaults, config);
+    
+    createMyZoomToExtentControl();
     
     // map initialisation
-    ms.map = new ol.Map({
+    map = new ol.Map({
       target: 'map',
       layers: [
         new ol.layer.Tile({
@@ -306,20 +323,66 @@ function service($compile){
         zoom: config.zoom
       }),
 		  controls: ol.control.defaults().extend([
-		    new MyZoomToExtentControl({tipLabel: "Fit to extent"}),
+		    new myZoomToExtentControl({tipLabel: "Fit to extent"}),
 				new ol.control.ScaleLine()
 			])
     });
     
-    ms.popupSetup();
-    ms.loadKML();
-    ms.zoomToExtend();
+    popupSetup();
+    loadKML();
+    zoomToExtend();
+  }
+
+  function createMyZoomToExtentControl(){
+    /**
+     * @constructor
+     * @extends {ol.control.Control}
+     * @param {Object=} opt_options Control options.
+     */
+    myZoomToExtentControl = function (opt_options) {
+
+      var options = opt_options || {};
+
+      var button = document.createElement('button');
+      button.id = 'zoom-to-extent';
+      button.setAttribute("title","Zoom to Extent");
+
+      var span = document.createElement('span');
+      
+      //span.setAttribute("class", "glyphicon glyphicon-record");
+      span.innerHTML = 'E';
+      button.appendChild(span);
+
+      var this_ = this;
+      var handler = function(e) {
+        e.preventDefault(); //cancel click event
+        zoomToExtend();
+        document.getElementById("zoom-to-extent").disabled = true;
+        setTimeout(function() {
+          document.getElementById("zoom-to-extent").disabled = false;
+        }, 1);
+      };
+
+      button.addEventListener('click', handler, true);
+      button.addEventListener('touchstart', handler, true);
+
+      var element = document.createElement('div');
+      element.className = 'zoom-to-extent ol-zoom-extent ol-unselectable ol-control';
+      element.appendChild(button);
+
+      ol.control.Control.call(this, {
+        element: element,
+        target: options.target
+      });
+
+    };
+    ol.inherits(myZoomToExtentControl, ol.control.ZoomToExtent);
   }
   
-  ms.zoomToExtend = function() {
+  function zoomToExtend() {
 		var bounds = ol.extent.createEmpty();
 		
-		 ms.map	//ol.Map
+		 map	//ol.Map
     	  .getLayers()	//ol.Collection
     	  .getArray()[1]	//ol.layer.Vector
     	  	.getSource()	//ol.source.KML
@@ -341,15 +404,15 @@ function service($compile){
 			
 			var animation = ol.animation.pan({
 				easing: eval(ol.easing.inAndOut),
-				source: ms.map.getView().getCenter()
+				source: map.getView().getCenter()
 			});
-			ms.map.beforeRender(animation);
+			map.beforeRender(animation);
 			
-			ms.map.getView().fitExtent(bounds10, ms.map.getSize());
+			map.getView().fitExtent(bounds10, map.getSize());
 		}	  
 	};
   
-  ms.loadKML = function(){
+  function loadKML(){
     var kml = '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://earth.google.com/kml/2.2"><Document><name><![CDATA[Coworking spaces]]></name><description><![CDATA[]]></description><Style id="style1"><IconStyle><Icon><href>http://geoklubb.se/foursquare-lists-kml-export/img/geoklubb-pin.png</href></Icon></IconStyle></Style><Placemark><name><![CDATA[Ziferblat]]></name><description><![CDATA[<a href="http://london.ziferblat.net">Venue URL</a><br />Phone: 07984 693440<br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.078374147415161,51.526985282303,0</coordinates></Point></Placemark><Placemark><name><![CDATA[Campus London]]></name><description><![CDATA[<a href="http://campuslondon.com">Venue URL</a><br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.085487365722656,51.522703131223,0</coordinates></Point></Placemark><Placemark><name><![CDATA[Rainmaking Loft]]></name><description><![CDATA[<a href="http://www.rainmakingloft.com">Venue URL</a><br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.073642730712891,51.50764732314,0</coordinates></Point></Placemark><Placemark><name><![CDATA[TechHub]]></name><description><![CDATA[Phone: 020 7490 0764<br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.087708234786987,51.525032831563,0</coordinates></Point></Placemark><Placemark><name><![CDATA[Innovation Warehouse London]]></name><description><![CDATA[<a href="http://innovationwarehouse.org">Venue URL</a><br />Phone: 020 7248 0199<br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.10269641876221,51.518998061413,0</coordinates></Point></Placemark><Placemark><name><![CDATA[Hub Westminster]]></name><description><![CDATA[<a href="http://westminster.impacthub.net">Venue URL</a><br />Phone: 020 7148 6720<br />]]></description><styleUrl>#style1</styleUrl><Point><coordinates>-0.13124592579464,51.507792367419,0</coordinates></Point></Placemark></Document></kml>';
     var kmlSource = new ol.source.KML({
         projection: 'EPSG:3857',
@@ -381,57 +444,11 @@ function service($compile){
     }
     
     // Add vectory layer to map
-    ms.map.addLayer(vectorLayer);
+    map.addLayer(vectorLayer);
     
     //render custom markers
-    ms.renderSVGFeatures();
+    renderSVGFeatures();
   }
-  
-	/**
-   * @constructor
-   * @extends {ol.control.Control}
-   * @param {Object=} opt_options Control options.
-   */
-  var MyZoomToExtentControl = function(opt_options) {
-
-    var options = opt_options || {};
-
-    var button = document.createElement('button');
-    button.id = 'zoom-to-extent';
-    button.setAttribute("title","Zoom to Extent");
-
-    var span = document.createElement('span');
-    
-    //span.setAttribute("class", "glyphicon glyphicon-record");
-    span.innerHTML = 'E';
-    button.appendChild(span);
-
-    var this_ = this;
-    var handler = function(e) {
-      e.preventDefault(); //cancel click event
-      ms.zoomToExtend();
-      document.getElementById("zoom-to-extent").disabled = true;
-      setTimeout(function() {
-        document.getElementById("zoom-to-extent").disabled = false;
-      }, 1);
-    };
-
-    button.addEventListener('click', handler, true);
-    button.addEventListener('touchstart', handler, true);
-
-    var element = document.createElement('div');
-    element.className = 'zoom-to-extent ol-zoom-extent ol-unselectable ol-control';
-    element.appendChild(button);
-
-    ol.control.Control.call(this, {
-      element: element,
-      target: options.target
-    });
-
-  };
-	ol.inherits(MyZoomToExtentControl, ol.control.ZoomToExtent);
-  
-  return ms;
 }
 
 
